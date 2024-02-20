d3c6da8fba30910903377ef77c8c76958f408107
Ayuth Mangmesap <ayuth.mang@gmail.com>
2024-02-20 16:04:19 +0700

    Sagas - Understanding Sagas
    // 🌍🌍🌍 CURL for Testing
    curl --location --request POST 'localhost:3000/alarms' \
    --header 'Content-Type: application/json' \
    --data-raw '{
        "name": "Alarm 1",
        "severity": "HIGH",
        "triggeredAt": "2021-01-01T00:00:00.000Z",
        "items": [
            {
                "name": "Item 1",
                "type": "TYPE_1"
            }
        ]
    }' | json_pp
    curl -X PATCH http://localhost:3000/alarms/___PASTE_YOUR_ID_FROM_AN_alarmId_IN_THE_PREVIOUS_CALL____HERE_____/acknowledge
    
    // ⚠️ remove | json_pp if your OS doesn't support it

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -4,9 +4,12 @@ import { AlarmsController } from '../presenters/http/alarms.controller';
 import { AlarmsService } from './alarms.service';
 import { AcknowledgeAlarmCommandHandler } from './commands/acknowledge-alarm.command-handler';
 import { CreateAlarmCommandHandler } from './commands/create-alarm.command-handler';
+import { NotifyFacilitySupervisorCommandHandler } from './commands/notify-facility-supervisor.command-handler';
 import { AlarmAcknowledgedEventHandler } from './event-handlers/alarm-acknowledged.event-handler';
 import { AlarmCreatedEventHandler } from './event-handlers/alarm-created.event-handler';
 import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
+import { CascadingAlarmsSaga } from './sagas/cascading-alarms.saga';
+import { UnacknowledgedAlarmsSaga } from './sagas/unacknowledged-alarms.saga';
 
 @Module({
   controllers: [AlarmsController],
@@ -16,8 +19,11 @@ import { GetAlarmsQueryHandler } from './queries/get-alarms.query-handler';
     CreateAlarmCommandHandler,
     GetAlarmsQueryHandler,
     AlarmCreatedEventHandler,
-    AcknowledgeAlarmCommandHandler, // 👈
-    AlarmAcknowledgedEventHandler, // 👈
+    AcknowledgeAlarmCommandHandler,
+    AlarmAcknowledgedEventHandler,
+    CascadingAlarmsSaga,
+    NotifyFacilitySupervisorCommandHandler,
+    UnacknowledgedAlarmsSaga,
   ],
 })
 export class AlarmsModule {
